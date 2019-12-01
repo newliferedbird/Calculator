3cf843aa52ca3a45c5acf09bfede8db5a3cdf000
newliferedbird <newliferedbird@gmail.com>
2019-12-01 23:17:04 +0300

    new commit

--- Calc v2/src/CalcMain.java
+++ Calc v2/src/CalcMain.java
@@ -3,61 +3,101 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class CalcMain {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String input = scan.nextLine();
-        StringBuilder output = new StringBuilder();
-        String[] strings = input.split("\\b");
-        Stack<String> stringStack = new Stack<>();
-        for (String string : strings) {
-            if (string.equals("+") || string.equals("-") || string.equals("*") || string.equals("/")) {
-                switch (string) {
+        String inputString = scan.nextLine();
+
+        String[] inArray = inputString.split("\\b");
+
+        Stack<String> inStack = new Stack<>();
+        Stack<String> outStack = new Stack<>();
+        Stack<String> result = new Stack<>();
+
+        for (String val : inArray) {
+            if (val.equals("+") || val.equals("-") || val.equals("*") || val.equals("/")) {
+
+                switch (val) {
                     case ("+"):
-                        try {
-                            if (stringStack.peek().equals("^") || stringStack.peek().equals("*") || stringStack.peek().equals("/")) {
-                                output.append(stringStack.pop());
-                                stringStack.add(string);
-                            } else {
-                                stringStack.add(string);
-                            }
-                        } catch (EmptyStackException e) {
-                            stringStack.add(string);
-                        }
-                        break;
-                    case ("-"):
-                        if (stringStack.peek().equals("^") || stringStack.peek().equals("*") || stringStack.peek().equals("/")) {
-                            output.append(stringStack.pop());
-                            stringStack.add(string);
-                        } else {
-                            stringStack.add(string);
-                        }
-                        break;
-                    case ("*"):
-                        if (stringStack.peek().equals("^") || stringStack.peek().equals("*") || stringStack.peek().equals("/")) {
-                            output.append(stringStack.pop());
-                            stringStack.add(string);
-                        } else {
-                            stringStack.add(string);
-                        }
-                        break;
                     case ("/"):
-                        if (stringStack.peek().equals("^") || stringStack.peek().equals("*") || stringStack.peek().equals("/")) {
-                            output.append(stringStack.pop());
-                            stringStack.add(string);
-                        } else {
-                            stringStack.add(string);
-                        }
+                    case ("-"):
+                    case ("*"):
+                        trycatch(inStack, outStack, val);
                         break;
                 }
-//                output += strings[i];
+
             } else {
-                output.append(string);
+                outStack.add(val);
             }
         }
-        while (stringStack.size() > 0) {
-            output.append(stringStack.pop());
+
+        formingOutStack(inStack, outStack);
+
+        Calculated(outStack, result);
+
+        System.out.println(Integer.parseInt(result.pop()));
+
+    }
+
+    public static void trycatch(Stack<String> inStack, Stack<String> outStack, String val) {
+
+        try {
+            if (inStack.peek().equals("^") || inStack.peek().equals("*") || inStack.peek().equals("/")) {
+                outStack.add(inStack.pop());
+                inStack.add(val);
+            } else {
+                inStack.add(val);
+            }
+        } catch (EmptyStackException e) {
+            inStack.add(val);
         }
-        System.out.println(output);
+    }
+
+    public static void formingOutStack(Stack<String> inStack, Stack<String> outStack) {
+        while (inStack.size() > 0) {
+            outStack.add(inStack.pop());
+        }
+    }
+
+    public static void Calculated(Stack<String> outStack, Stack<String> result) {
+
+        while (outStack.size() > 0) {
+            if (outStack.peek().equals("+") || outStack.peek().equals("-") || outStack.peek().equals("*") || outStack.peek().equals("/")) {
+                switch (outStack.peek()) {
+                    case ("+"):
+                        int x1 = Integer.parseInt(result.pop());
+                        int x2 = Integer.parseInt(result.pop());
+                        int finalresult1 = x1 + x2;
+                        result.add(Integer.toString(finalresult1));
+                        break;
+                    case ("-"):
+                        int x3 = Integer.parseInt(result.pop());
+                        int x4 = Integer.parseInt(result.pop());
+                        int finalresult2 = x3-x4;
+                        result.add(Integer.toString(finalresult2));
+                        break;
+                    case ("*"):
+                        int x5 = Integer.parseInt(result.pop());
+                        int x6 = Integer.parseInt(result.pop());
+                        int finalresult3 = x5*x6;
+                        result.add(Integer.toString(finalresult3));
+                        break;
+                    case ("/"):
+                        int x7 = Integer.parseInt(result.pop());
+                        int x8 = Integer.parseInt(result.pop());
+                        int finalresult4 = x7/x8;
+                        result.add(Integer.toString(finalresult4));
+                        break;
+                }
+            }
+            else {
+                result.add(outStack.pop());
+            }
+        }
     }
 }
-//if(stringStack.search("/")!=-1)
+
+
+
+
